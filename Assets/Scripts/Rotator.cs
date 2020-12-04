d87b73771402c8491a3ac0495b34935b81a78018
SkylarFiaes <skylarsdf@hotmail.com>
2020-12-04 12:39:28 +0000

    Added Fish and took some Icebergs. Script to rotate your objects (if you want) added.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public int rotateX, rotateY, rotateZ;
+    void Update () 
+    {
+        transform.Rotate (new Vector3 (rotateX, rotateY, rotateZ) * Time.deltaTime);
+    }
+}

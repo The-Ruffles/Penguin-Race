918e47818bae746c7a1e0ccd37fdf762e2cdfdf0
SamTruesdale <samtruesdale10@gmail.com>
2020-12-08 23:48:27 +0000

    main menu done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    void start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    } 
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void KyleLevel()
+    {
+        SceneManager.LoadScene("KyleScene");
+    }
+
+    public void SkylarLevel()
+    {
+        SceneManager.LoadScene("SkylarScene");
+    }
+
+    public void AinohaLevel()
+    {
+        SceneManager.LoadScene("Ainoha Scene");
+    }
+
+    public void RyanLevel()
+    {
+        SceneManager.LoadScene("Ryan Scene");
+    }
+
+    public void SamLevel()
+    {
+        SceneManager.LoadScene("Sam Scene");
+    }
+}
